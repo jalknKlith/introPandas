81332a3e82d63cd785f8e134766f45a6fc401463
jalknKlith <169000561+jalknKlith@users.noreply.github.com>
2024-10-16 08:35:03 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -4,17 +4,14 @@ data = {
   "calories": [420, 380, 390],
   "duration": [50, 40, 45]
 }
-print(type(data))
+
 #load data into a DataFrame object:
 df = pd.DataFrame(data)
 
 print(df)
-print(type(df))
 
 #locate Row
 print(df.loc[0])
-
-print(' ')
 
 #use a list of indexes
 print(df.loc[[0, 1]])
@@ -23,7 +20,6 @@ print(df.loc[[0, 1]])
 dfn = pd.DataFrame(data, index = ["day1", "day2", "day3"])
 
 print(dfn)
-print(type(dfn))
 
 #located named indexes
 print(dfn.loc["day2"])
