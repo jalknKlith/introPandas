e135d6db0fb6da82f80f32fc82cc868472d665e8
jalknKlith <169000561+jalknKlith@users.noreply.github.com>
2024-10-16 09:02:40 -0500

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -1,25 +1,27 @@
 import pandas as pd 
 
 data = {
-  "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
+  "calorias": [420, 380, 390],
+  "duracion": [50, 40, 45]
 }
 
-#load data into a DataFrame object:
+#cargar todos los datos en la tabla
 df = pd.DataFrame(data)
 
+#imprimir la tabla
 print(df)
 
-#locate Row
+#imprimir la fila uno
 print(df.loc[0])
 
-#use a list of indexes
+#imprimir la fila uno, y dos
 print(df.loc[[0, 1]])
 
-#give each row a name
+#nombrar cada fila
 dfn = pd.DataFrame(data, index = ["day1", "day2", "day3"])
 
+#imprimir la tabla
 print(dfn)
 
-#located named indexes
+#localizar la duracion del dia dos
 print(dfn.loc["day2"])
