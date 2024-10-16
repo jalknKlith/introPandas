e9c26e4aaf9e478a9903371206fdf7b746981087
jalknKlith <169000561+jalknKlith@users.noreply.github.com>
2024-10-16 08:36:51 -0500

    Update readCSV.py

--- readCSV.py
+++ readCSV.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #Increase the maximum number of rows
-#pd.options.display.max_rows = 200
+pd.options.display.max_rows = 200
 
 df = pd.read_csv('data.csv')
 
@@ -9,5 +9,5 @@ df = pd.read_csv('data.csv')
 print(df)
 
 #print entire dataframe
-#print(df.to_string())
-print(type(df))
+print(df.to_string())
+
